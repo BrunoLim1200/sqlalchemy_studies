f3ff8700ea7bb160d68377c2b694de6f6a8fb388
BrunoLim1200 <bruno.lisa1200@gmail.com>
2020-07-27 16:23:19 -0400

    First commit from notes file to SQLAlchemy

---
+++ test.py
@@ -0,0 +1,27 @@
+from models import People, db_session
+
+def new_peoples():
+    people = People(name='Jorge',age=22)
+    print(people)
+    people.save()
+
+def check_peoples():
+    people = People.query.all()
+    print(people)
+    #people = People.query.filter_by(name='Jorge').first()
+    #print(people.age)
+
+def change_peoples():
+    people = People.query.filter_by(name='Jorge').first()
+    people.age = 21
+    people.save()
+
+def delete_peoples():
+    people = People.query.filter_by(name='Jorge').first()
+    people.delete()
+
+if __name__ == '__main__':
+    #new_peoples()
+    #delete_peoples()
+    check_peoples()
+    #change_peoples()
